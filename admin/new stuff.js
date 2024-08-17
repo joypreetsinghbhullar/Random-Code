74c6d34053b30537352e117881617791d342754a
joypreetsinghbhullar <jo-jojoy@outlook.com>
2024-08-17 23:09:53 +0930

    Update new stuff.js

--- admin/new stuff.js
+++ admin/new stuff.js
@@ -1,43 +1,41 @@
-input.onButtonPressed(Button.B, function () {
+def on_button_pressed_b():
+    global output, dialacurry, mcdonald, dominos
     output = randint(0, 2)
-    if (output == 0) {
+    if output == 0:
         dialacurry = randint(0, 3)
-        basic.showString("dial a curry")
-        if (dialacurry == 0) {
-            basic.showString("Tandoori Butter Chicken")
-        } else if (dialacurry == 1) {
-            basic.showString("Roti")
-        } else if (dialacurry == 2) {
-            basic.showString("Veg Spring Roll")
-        }
-    } else if (output == 1) {
+        basic.show_string("dial a curry")
+        if dialacurry == 0:
+            basic.show_string("Tandoori Butter Chicken")
+        elif dialacurry == 1:
+            basic.show_string("Roti")
+        elif dialacurry == 2:
+            basic.show_string("Veg Spring Roll")
+    elif output == 1:
         mcdonald = randint(0, 2)
-        basic.showString("mcdonald")
-        if (mcdonald == 0) {
-            basic.showString("McFlurry")
-        } else if (mcdonald == 1) {
-            basic.showString("Burger")
-        } else if (mcdonald == 2) {
-            basic.showString("French Fries")
-        }
-    } else if (output == 2) {
+        basic.show_string("mcdonald")
+        if mcdonald == 0:
+            basic.show_string("McFlurry")
+        elif mcdonald == 1:
+            basic.show_string("Burger")
+        elif mcdonald == 2:
+            basic.show_string("French Fries")
+    elif output == 2:
         dominos = randint(0, 2)
-        basic.showString("Dominos")
-        if (dominos == 0) {
-            basic.showString("Dominos")
-        } else if (dominos == 1) {
-            basic.showString("Garlic Bread")
-        } else if (dominos == 2) {
-            basic.showString("Spicy Peepy Paneer")
-        } else if (dominos) {
-            basic.showString("Pepporoni Pizza")
-        }
-    }
-})
-// Settings
-let dominos = 0
-let mcdonald = 0
-let dialacurry = 0
-let output = 0
-basic.showString("CHOOSE RESTAURANT")
-basic.showString("Press on button B to find a random restaurant and a food item from theree!")
+        basic.show_string("Dominos")
+        if dominos == 0:
+            basic.show_string("Dominos")
+        elif dominos == 1:
+            basic.show_string("Garlic Bread")
+        elif dominos == 2:
+            basic.show_string("Spicy Peepy Paneer")
+        elif dominos:
+            basic.show_string("Pepporoni Pizza")
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+# Settings
+dominos = 0
+mcdonald = 0
+dialacurry = 0
+output = 0
+basic.show_string("CHOOSE RESTAURANT")
+basic.show_string("Press on button B to find a random restaurant and a food item from theree!")
